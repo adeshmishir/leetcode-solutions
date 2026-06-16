b6ac788a2ef6de699309d5c2ad5413e5bd8bec9b
Adesh Mishra <mishraadesh770@gmail.com>
2026-06-16 14:14:43 +0530

    Time: 4 ms (63.64%), Space: 39.9 MB (42.05%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string ans="";
+        int n =s.length();
+        for(int i=0;i<n;i++){
+            if(s[i]=='*'){
+                if(ans.size()!=0){
+                    ans.pop_back();
+                }
+            }
+           else if(s[i]=='#'){
+                ans+=ans;
+            }
+           else if(s[i]=='%'){
+                reverse(ans.begin(),ans.end());
+            }
+            else ans.push_back(s[i]);
+        }
+        return ans;
+    }
+};
